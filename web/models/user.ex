a8760d184c0e3ad2dccbf8b0c48ae914b2c1cda3
Marc Riera <mrc2407@gmail.com>
2016-04-15 10:14:21 +0200

    Hash passwords for new users

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,12 +1,6 @@
 defmodule Rumbl.User do
   use Rumbl.Web, :model
 
-  def changeset(model, params \\ :empty) do
-    model
-    |> cast(params, ~w(name username), [])
-    |> validate_length(:username, min: 1, max: 20)
-  end
-
   schema "users" do
     field :name, :string
     field :username, :string
@@ -14,5 +8,30 @@ defmodule Rumbl.User do
     field :password_hash, :string
 
     timestamps
+  end
+
+  # Controls what parameters are accepted from a form submission and validates
+  # them. `params` is `:empty` by default in order to distinguish a blank new
+  # changeset from an empty form submission.
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), [])
+    |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ -> changeset
+    end
   end
 end
